91e484f755d53fc67039b9594bc4e871d836f905
gabicastrum <gabriela.laurindo@dbserver.com.br>
2025-04-15 16:57:00 -0300

    feat(entendendo-fluxo-dados): criando formulário, property binding, botões e event binding

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado!")
+    }
+
+  cancelar() {
+    alert("Pensamento cancelado!")
+  }
+
+}
